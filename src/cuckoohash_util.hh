toojays/libcuckoo

commit fe3728d59831266ff4dedc0752ed5e682c302632

Added a thread_local compatibility macro
thread_local is not supported in g++-4.7 and some versions of clang, so
we add a macro for that. Also updated the README with the correct clang
version number.

src/cuckoohash_util.hh
@@ -9,9 +9,10 @@
 #include "cuckoohash_config.hh" // for LIBCUCKOO_DEBUG
 
 #if LIBCUCKOO_DEBUG
-#  define LIBCUCKOO_DBG(fmt, ...)                                        \
-     fprintf(stderr, "\x1b[32m""[libcuckoo:%s:%d:%lu] " fmt"" "\x1b[0m", \
-             __FILE__,__LINE__, (unsigned long)std::this_thread::get_id(), __VA_ARGS__)
+#  define LIBCUCKOO_DBG(fmt, ...)                                          \
+     fprintf(stderr, "\x1b[32m""[libcuckoo:%s:%d:%lu] " fmt"" "\x1b[0m",   \
+             __FILE__,__LINE__, (unsigned long)std::this_thread::get_id(), \
+             __VA_ARGS__)
 #else
 #  define LIBCUCKOO_DBG(fmt, ...)  do {} while (0)
 #endif
@@ -34,6 +35,17 @@
 #define LIBCUCKOO_SQUELCH_PADDING_WARNING __pragma(warning(suppress : 4324))
 #else
 #define LIBCUCKOO_SQUELCH_PADDING_WARNING
+#endif
+
+/**
+ * thread_local requires GCC >= 4.8 and is not supported in some clang versions,
+ * so we use __thread if thread_local is not supported
+ */
+#if ((__GNUC__ && !__clang__ && __GNUC__ == 4 && __GNUC_MINOR__ < 8) || \
+     (__clang__ && !__has_feature(cxx_thread_local)))
+#define LIBCUCKOO_THREAD_LOCAL __thread
+#else
+#define LIBCUCKOO_THREAD_LOCAL thread_local
 #endif
 
 // For enabling certain methods based on a condition. Here's an example.

src/cuckoohash_map.hh
@@ -324,7 +324,7 @@ private:
     static inline int get_counterid() {
         // counterid stores the per-thread counter index of each thread. Each
         // counter value corresponds to a core on the machine.
-        static thread_local int counterid = -1;
+        static LIBCUCKOO_THREAD_LOCAL int counterid = -1;
 
         if (counterid < 0) {
             counterid = rand() % kNumCores();

commit cbca13491d0d28d68ec98d26627080fc325ecf45

Fix GCC build breakage when defining LIBCUCKOO_THREAD_LOCAL.
fe3728d5 broke the GCC build like:

In file included from ../../src/cuckoohash_map.hh:30:0,
                 from insert_throughput.cc:24:
../../src/cuckoohash_util.hh:45:34: error: missing binary operator before token "("
      (__clang__ && !__has_feature(cxx_thread_local)))
                                  ^
Under GCC, "__clang__ && !__has_feature(cxx_thread_local)" would evaluate to
"0 && !0(0)", which is syntactically bogus.

This commit rewrites the logic used to define LIBCUCKOO_THREAD_LOCAL such that
__has_feature() is only evaluated when building with Clang.

src/cuckoohash_util.hh
@@ -41,11 +41,17 @@
  * thread_local requires GCC >= 4.8 and is not supported in some clang versions,
  * so we use __thread if thread_local is not supported
  */
-#if ((__GNUC__ && !__clang__ && __GNUC__ == 4 && __GNUC_MINOR__ < 8) || \
-     (__clang__ && !__has_feature(cxx_thread_local)))
-#define LIBCUCKOO_THREAD_LOCAL __thread
-#else
 #define LIBCUCKOO_THREAD_LOCAL thread_local
+#if defined(__clang__)
+#  if !__has_feature(cxx_thread_local)
+#    undef LIBCUCKOO_THREAD_LOCAL
+#    define LIBCUCKOO_THREAD_LOCAL __thread
+#  endif
+#elif defined(__GNUC__)
+#  if __GNUC__ == 4 && __GNUC_MINOR__ < 8
+#    undef LIBCUCKOO_THREAD_LOCAL
+#    define LIBCUCKOO_THREAD_LOCAL __thread
+#  endif
 #endif
 
 // For enabling certain methods based on a condition. Here's an example.